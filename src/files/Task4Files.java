cc1d20a6f05822f542c41fa1f3922b68b513ae2a
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-07-16 12:50:11 +0200

    Update

---
+++ src/files/Task4Files.java
@@ -0,0 +1,34 @@
+package files;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task4Files {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String kindToCreate;
+        do {
+            System.out.println("1 - Utwórz plik");
+            System.out.println("2 - Utwórz katalog");
+            System.out.println("3 - Wyjdz");
+            System.out.print("Wybierz: ");
+            kindToCreate = scanner.nextLine();
+
+            System.out.print("Podaj path: ");
+            String path = scanner.nextLine();
+
+            File file = new File(path);
+            if(kindToCreate.equals("1")){
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }else if(kindToCreate.equals("2")){
+                file.mkdirs();
+            }
+        }while (!kindToCreate.equals("3"));
+    }
+}
